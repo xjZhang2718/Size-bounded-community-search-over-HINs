1c507c6d7a0184fef82b0aecc7c8d7fcdbe569ea
xjZhang2718 <zhangxinjian9@gmail.com>
2026-01-26 09:57:10 +1100

    Update kv calculation to use maximum trussness value

--- enumerateEdge.py
+++ enumerateEdge.py
@@ -432,7 +432,7 @@ def heuristic(q,s):
             if not trussness[i]:
                 kv = 0
             else:
-                kv = min(trussness[i].values())
+                kv = max(trussness[i].values())
             if kv<minkv:
                 minkv = kv
                 vk = i
